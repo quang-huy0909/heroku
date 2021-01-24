baea1fbc35c01ef42f0e6ea52f1bc0ac8b35f45f
quang-huy0909 <77915699+quang-huy0909@users.noreply.github.com>
2021-01-24 15:46:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
 <H1>
  Hiiiiii
 </H1>
-<img src ="https://github.com/quang-huy0909/heroku/blob/main/Koala.jpg" alt="Koala"/>
+<img src ="https://user-images.githubusercontent.com/77915699/105625350-3d650e80-5e5b-11eb-813e-4a723d86d603.jpg" alt="Koala"/>
 </body>
 </html>
